1c89cd09e79757966f7bfed1b43a854ae9de53f0
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-07-12 20:38:31 +0200

    setup

--- lua/ember/init.lua
+++ lua/ember/init.lua
@@ -1,7 +1,22 @@
-local function setup()
-	print("hello")
+local commands = require 'ember.commands'
+local pickers = require 'ember.pickers'
+local tools = require 'ember.tools'
+
+local M = {}
+
+---@class opts
+---@field picker? picker Which picker to use, defaults to "quickfix"
+
+-- Setup function for the module
+---@param opts opts
+function M.setup(opts)
+  local picker_type = opts.picker or 'quickfix'
+  local picker = pickers.get_picker(picker_type)
+  commands.register_commands(picker)
+
+  M.get_related_files = function()
+    tools.get_related_files(picker)
+  end
 end
 
-return {
-	setup = setup,
-}
+return M
